b29e284d0e501b3263587f3687931282c2c83b6d
kuchurisatwik <kuchurisatwik@gmail.com>
2025-11-13 15:24:40 +0530

    Initial commit

---
+++ createInstance.tf
@@ -0,0 +1,5 @@
+
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = "ami-05b05ba1e8bceef76"
+  instance_type = "t3.micro"
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = var.Access_key
+  secret_key = var.Secret_key
+  region     = "us-east-1"
+}
