9d895c5bac0acdffa7704ad9918b23f362a5aae1
kuchurisatwik <kuchurisatwik@gmail.com>
2025-11-14 15:40:57 +0530

    simple and cleam

---
+++ terraform variables detailed explanation/Demo DataSource/createinstance.tf
@@ -0,0 +1,13 @@
+
+
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.levelup_key.key_name
+
+  tags = {
+    Name = "custom_instance"
+  }
+
+ 
+
